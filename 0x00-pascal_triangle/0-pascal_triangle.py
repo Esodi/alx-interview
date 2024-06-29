3bd57d2c10b1e10ae66b096fbebfe13841d789b3
Method Mkoma <gershommethod02@gmail.com>
2024-06-29 15:46:22 +0300

    task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    big = []
+    if n <= 0:
+        return big
+    else:
+        lst = [1]
+        for j in range(n):
+            lst1 = []
+            x = 0
+            y = 1
+            lst1.append(1)
+            for k in range(j):
+                e = lst[x] + lst[y]
+                lst1.append(e)
+                x += 1
+                y += 1
+            lst1.append(1)
+            big.append(lst)
+            lst = lst1.copy()
+    return big
