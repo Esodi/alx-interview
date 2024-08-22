8a9d8538231ab4d9e510ffbb974443122d633ab6
Method Mkoma <gershommethod02@gmail.com>
2024-08-22 23:38:25 +0300

    task 0 fixing

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,6 +3,7 @@
  method that determines if a given data set represents a valid UTF-8 encoding.
 '''
 
+
 def validUTF8(data):
     ''' the function itself'''
     num_byt = 0
@@ -14,11 +15,10 @@ def validUTF8(data):
                 num_byt = 2
             elif i >> 3 == 0b11110:
                 num_byt = 3
-            elif i >> 7 == 0:
-                continue
+            elif i >> 7 != 0:
+                return False
         else:
             if i >> 6 != 0b10:
                 return False
             num_byt -= 1
-            
     return num_byt == 0
