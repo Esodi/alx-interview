c2059f80236264f4099abc2683c513828f5f9610
Method Mkoma <gershommethod02@gmail.com>
2024-10-27 12:43:12 +0300

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' making change '''
+
+
+def makeChange(coins, total):
+    '''making change'''
+    if total <= 0:
+        return 0
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    return dp[total] if dp[total] != total + 1 else -1
